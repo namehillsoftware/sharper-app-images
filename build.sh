36a9b7e81b57cc6f31c0c3b91a7ccf571426c108
David <dvedvick@gmail.com>
2024-10-08 21:38:50 -0500

    Added main build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 rm -rf _out
 
 # Generate a random ID for the build, to be used for finding the build in the docker host.
@@ -9,13 +11,13 @@ BUILD_ID="$(od  -vN "8" -An -tx1  /dev/urandom | tr -d " \n")"
 echo "Build ID: ${BUILD_ID}"
 
 docker buildx build --tag "${BUILD_ID}" . && docker run --name "${BUILD_ID}" --privileged "${BUILD_ID}"
+
+EXIT_CODE=$?
  	
 BUILD_CONTAINER=${BUILD_ID}
 
 docker container cp "${BUILD_CONTAINER}":/out ./_out
 docker container rm "${BUILD_CONTAINER}" -v
 docker image rm "${BUILD_ID}"
-    
-EXIT_CODE=${PIPESTATUS[0]}
 
 exit "$EXIT_CODE"
